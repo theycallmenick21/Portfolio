dd7e3e8ac711d0e07373c17fb741abda931f42a8
Nicolas Ibañez <nibanezpaez@gmail.com>
2024-12-02 17:50:29 -0500

    add info

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,14 +2,15 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { PersonalInfoCardComponent } from '../../src/components/personal-info-card/personal-info-card.component';
 import { MainInfoComponent } from '../../src/components/main-info/main-info.component';
-
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   imports: [
     RouterOutlet,
     PersonalInfoCardComponent,
-    MainInfoComponent],
+    MainInfoComponent,
+    CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
